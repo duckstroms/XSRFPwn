15ec1da2cd70730cd986a4dbcea3ea94deab45cc
Duck Researcher <57305361+duckstroms@users.noreply.github.com>
2021-06-15 21:42:09 +0800

    __init__.py

---
+++ xsrfpwn/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#-:-:-:-:-:-:-::-:-:#
+#    XSRF Pwn     #
+#-:-:-:-:-:-:-::-:-:#
+
+# Author: duckstroms
+# This module requires XSRFPwn
+# https://github.com/duckstroms/XSRFPwn
+
+__version__ = '2.3.1'
+__license__ = 'GNU General Public License v3 (GPLv3)'
